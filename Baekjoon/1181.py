ebe6c4432f83466566b5c3e09bf9acbd1eabb168
hyunsu <cjswkans1224@gmail.com>
2023-09-19 22:23:49 +0900

    230919

--- Baekjoon/1181.py
+++ Baekjoon/1181.py
@@ -1,9 +1,9 @@
 # 1181. 단어정렬
 N = int(input())
-arr = []
-for _ in range(N):
-    word = input()
-    arr.append(word)
-words = sorted(arr, key=lambda x: (len(x), x))
-for word in words:
-    print(word)
+words = [str(input()) for i in range(N)]
+words = list(set(words))
+words.sort()
+words.sort(key=len)
+
+for i in words:
+    print(i)
