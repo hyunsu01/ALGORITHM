f464c4c544cd2241ea9ef8ea222574a414d18221
hyunsu <cjswkans1224@gmail.com>
2023-10-10 10:49:54 +0900

    231009

--- Programmers/Lv0/Lv0_외계행성의 나이.py
+++ Programmers/Lv0/Lv0_외계행성의 나이.py
@@ -2,7 +2,7 @@
 def solution(age):
     dic = {'0':'a', '1':'b', '2':'c', '3':'d', '4':'e', '5':'f',
             '6':'g', '7':'h', '8':'i', '9':'j'}
-    
+    answer = ''
     for i in str(age):
         answer+=(dic[i])
     return answer
