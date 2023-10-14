4f10fce1ee6ac7e558514e41cfd851b7d37b52d5
hyunsu <cjswkans1224@gmail.com>
2023-10-14 22:58:51 +0900

    231014

---
+++ Programmers/Lv0/모스부호.py
@@ -0,0 +1,17 @@
+def solution(letter):
+    morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    words = letter.split(' ')
+    answer = []
+    for word in words:
+        answer.append(morse.get(word, ''))
+    
+    answer = ''.join(answer)
+    return answer
+
+solution(".... . .-.. .-.. ---")
